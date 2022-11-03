20ef8486a82137bfdf92567a160a83df6e648ab3
drex09 <cruzdrexel@gmail.com>
2022-11-03 19:32:14 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import './App.css';
+import IMG from './naruto.webp'
+
+function App() {
+
+
+ 
+
+  return (
+    <div className="container">
+       <img src={IMG} alt="horse" />
+
+      <div>
+        <h1 id='hell' className='headertext'>Hello World!</h1>
+      <p>gfffg<span>uuuu</span></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
